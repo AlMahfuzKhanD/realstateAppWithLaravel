471106daa0c998a27bafa505cdfae4caa560351e
mahfuz380 <almahfuz380@gmail.com>
2023-12-03 00:18:35 +0600

    dummy data set with seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +15,34 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            //Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123456'),
+                'role' => 'admin',
+                'status' => 'active'
+            ],
+            // Agent
+            [
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('123456'),
+                'role' => 'agent',
+                'status' => 'active'
+            ],
+            // User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('123456'),
+                'role' => 'user',
+                'status' => 'active'
+            ]
+            ]);
     }
 }
