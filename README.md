aabc3e37a6a9cf152a38a646eca1b1fb5fbdcb26
mahfuz380 <almahfuz380@gmail.com>
2023-12-03 13:01:43 +0600

    role middleware added so that user cannot access to admin

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Steps to be followed to develope this projects
 5) Migrate with seed
 6) Create Controller and Dashboard for Admin, agent and User
 7) Modify AuthinticatedSessionController to redirect Admin,agent and user dashboard.
+8) Create Role Middleware ,register it in carnel and set middleware so that user can not login to agent and admin.
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
-Route::get('/agent/dashboard',[AgentController::class,'AgentDashboard'])->name('agent.dashboard');
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+});
+Route::middleware(['auth','role:agent'])->group(function(){
+    Route::get('/agent/dashboard',[AgentController::class,'AgentDashboard'])->name('agent.dashboard');
+});
