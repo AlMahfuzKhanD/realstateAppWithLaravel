184cb44fe7d5f95d1f720a7f2c88bf883160aee0
mahfuz380 <almahfuz380@gmail.com>
2023-12-03 12:41:04 +0600

    Modify AuthinticatedSessionController to redirect Admin,agent and user dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,8 +28,17 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+        $url = '';
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        if($request->user()->role === 'admin'){
+            $url = '/admin/dashboard';
+        }else if($request->user()->role === 'agent'){
+            $url = '/agent/dashboard';
+        }else if($request->user()->role === 'agent'){
+            $url = '/dashboard';
+        }
+
+        return redirect()->intended($url);
     }
 
     /**
