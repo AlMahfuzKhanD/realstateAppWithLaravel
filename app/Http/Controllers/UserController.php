e781448284fcb59aa193eaaef4bc9ebd8b8f0e22
mahfuz380 <almahfuz380@gmail.com>
2023-12-06 23:18:56 +0600

    front end basic segmantation done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        return view('frontend.frontend_dashboard');
+        return view('frontend.index');
     } // end of index
 }
---
+++ resources/views/frontend/home/preload.blade.php
@@ -0,0 +1,36 @@
+<div class="loader-wrap">
+    <div class="preloader">
+        <div class="preloader-close"><i class="far fa-times"></i></div>
+        <div id="handle-preloader" class="handle-preloader">
+            <div class="animation-preloader">
+                <div class="spinner"></div>
+                <div class="txt-loading">
+                    <span data-text-preloader="A" class="letters-loading">
+                       A
+                    </span>
+                    <span data-text-preloader="L" class="letters-loading">
+                        L
+                    </span>
+                    <span data-text-preloader="M" class="letters-loading">
+                        M
+                    </span>
+                    <span data-text-preloader="A" class="letters-loading">
+                        A
+                    </span>
+                    <span data-text-preloader="H" class="letters-loading">
+                       H
+                    </span>
+                    <span data-text-preloader="F" class="letters-loading">
+                        F
+                    </span>
+                    <span data-text-preloader="U" class="letters-loading">
+                        U
+                    </span>
+                    <span data-text-preloader="J" class="letters-loading">
+                        J
+                    </span>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
