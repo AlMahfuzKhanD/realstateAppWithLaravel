7af474d44512907f323f573e8056acd5a8dbb40a
mahfuz380 <almahfuz380@gmail.com>
2023-12-31 12:28:17 +0600

    agent login register done

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class AgentController extends Controller
 {
     public function AgentDashboard(){
         return view('agent.agent_dashboard');
     } // End of AdminDashboard
+
+    public function AgentLogin(){
+        return view('agent.agent_login');
+    } // End of AgentLogin
+
+    public function AgentRegister(Request $request){
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => 'agent',
+            'status' => 'inactive',
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::AGENT);
+    } // End of AgentLogin
 }
--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,8 @@ Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard',[AgentController::class,'AgentDashboard'])->name('agent.dashboard');
 });
 
+Route::get('/agent/login',[AgentController::class,'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
+Route::post('/agent/register',[AgentController::class,'AgentRegister'])->name('agent.register');
 
 Route::middleware(['auth','role:admin'])->group(function(){
     // Property Type All Route
