b66ae190ce606508d520a803056acd9053c041dd
mahfuz380 <almahfuz380@gmail.com>
2024-01-09 12:16:50 +0600

    wishlist file setup done

--- resources/views/frontend/frontend_dashboard.blade.php
+++ resources/views/frontend/frontend_dashboard.blade.php
@@ -9,6 +9,7 @@
         <script src="{{ asset('backend/assets/js/jquery-3.7.1.min.js') }}"></script>
         <!-- Fav Icon -->
         <link rel="icon" href="{{ asset('frontend/assets/images/favicon.ico') }}" type="image/x-icon" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
@@ -105,6 +106,17 @@
             break; 
         }
         @endif 
+        </script>
+    <!-- Add to wish list -->
+        <script>
+            $.ajaxSetup({
+                headers:{
+                    'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+                }
+            })
+            function addToWishList(property_id){
+
+            }
         </script>
     </body>
     <!-- End of .page_wrapper -->
--- resources/views/frontend/home/feature.blade.php
+++ resources/views/frontend/home/feature.blade.php
@@ -45,7 +45,7 @@
                                             <a href="property-details.html"><i class="icon-12"></i></a>
                                         </li>
                                         <li>
-                                            <a href="property-details.html"><i class="icon-13"></i></a>
+                                            <a aria-label="Add to wish list" class="action-btn" id="{{ $item->id }}" onclick="addToWishList(this.id)"><i class="icon-13"></i></a>
                                         </li>
                                     </ul>
                                 </div>
