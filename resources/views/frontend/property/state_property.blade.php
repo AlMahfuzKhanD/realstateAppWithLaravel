58a6aeaec52b8837bb432972cc654b56c6d1bc3d
mahfuz380 <almahfuz380@gmail.com>
2024-01-29 18:01:16 +0600

    url issue fixed in all details page

--- resources/views/frontend/property/state_property.blade.php
+++ resources/views/frontend/property/state_property.blade.php
@@ -131,10 +131,10 @@
                                                         <figure class="image"><img src="{{ asset($item->property_thumbnail) }}" alt="" style="width:350px;height:350px;"></figure>
                                                         <div class="batch"><i class="icon-11"></i></div>
                                                         <span class="category">{{ $item->featured == 1? 'Featured' : 'New' }}</span>
-                                                        <div class="buy-btn"><a href="property-details.html">For {{ $item->property_status??'' }}</a></div>
+                                                        <div class="buy-btn"><a href="{{ $item->property_status == 'rent' ?  route('rent.property') : route('buy.property')  }}">For {{ $item->property_status??'' }}</a></div>
                                                     </div>
                                                     <div class="lower-content">
-                                                        <div class="title-text"><h4><a href="property-details.html">{{ $item->property_name??'' }}</a></h4></div>
+                                                        <div class="title-text"><h4><a href="{{ url('property/details/'.$item->id.'/'.$item->property_slug) }}">{{ $item->property_name??'' }}</a></h4></div>
                                                         <div class="price-box clearfix">
                                                             <div class="price-info pull-left">
                                                                 <h6>Start From</h6>
