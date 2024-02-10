afd816b4b2fd1e164e8a70c9c83504a1b1dda4d2
mahfuz380 <almahfuz380@gmail.com>
2024-02-10 13:38:59 +0600

    live chat user frontend done

--- resources/js/components/SendMessage.vue
+++ resources/js/components/SendMessage.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
-        <h1> check vue installation</h1>
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#chat_now">
+  Chat Now
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="chat_now" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title fs-5" id="exampleModalLabel">Chat With</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="">
+      <div class="modal-body">
+        <div class="form-group">
+            <textarea class="form-control" name="" id="" cols="30" rows="3" placeholder="Type Your Message">
+
+            </textarea>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send Message</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
     </div>
 </template>
